f942699bcf1af3e01b8929aa1048e6ce58ecdaa2
hooran-gallery <q1297504@gmail.com>
2026-06-17 10:07:31 +0330

    Update server.js

--- server.js
+++ server.js
@@ -1,68 +1,37 @@
 const express = require("express");
-const fs = require("fs");
+const http = require("http");
+const { Server } = require("socket.io");
 
 const app = express();
 
-app.use(express.json());
+const server = http.createServer(app);
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "*");
-    res.header("Access-Control-Allow-Methods", "GET,POST");
-    next();
-});
-
-// تست سرور
-app.get("/", (req, res) => {
-    res.send("Server Online");
-});
-
-// تست ثبت نام
-app.get("/register", (req, res) => {
-    res.send("Register Route Works");
-});
-
-// ثبت نام
-app.post("/register", (req, res) => {
-
-    const { username, password } = req.body;
-
-    let users = [];
-
-    if (fs.existsSync("users.json")) {
-        users = JSON.parse(
-            fs.readFileSync("users.json", "utf8")
-        );
+const io = new Server(server,{
+    cors:{
+        origin:"*"
     }
+});
 
-    const exist = users.find(
-        user => user.username === username
-    );
+app.get("/",(req,res)=>{
+    res.send("MiniChat Online");
+});
 
-    if (exist) {
-        return res.json({
-            success: false,
-            message: "این نام کاربری قبلاً ثبت شده است"
-        });
-    }
+io.on("connection",(socket)=>{
 
-    users.push({
-        username,
-        password
+    console.log("User Connected");
+
+    socket.on("send-message",(data)=>{
+
+        io.emit("new-message",data);
+
     });
 
-    fs.writeFileSync(
-        "users.json",
-        JSON.stringify(users, null, 2)
-    );
-
-    res.json({
-        success: true,
-        message: "ثبت نام موفق"
+    socket.on("disconnect",()=>{
+        console.log("User Left");
     });
 
 });
 
-app.listen(process.env.PORT || 3000, () => {
+server.listen(process.env.PORT || 3000,()=>{
     console.log("Server Started");
 });
